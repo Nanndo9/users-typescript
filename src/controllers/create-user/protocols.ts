a716a91b264ef5489f10cdcd40c04ddcaca55687
Nanndo9 <100873856+Nanndo9@users.noreply.github.com>
2024-07-27 00:18:20 -0300

    feat: add MongoCreateRepository

---
+++ src/controllers/create-user/protocols.ts
@@ -0,0 +1,11 @@
+import { User } from "../../models/user";
+export interface createUserParams {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+}
+
+export interface ICreateUserRepostirory {
+    createUser(params:createUserParams): Promise<User>;
+}
