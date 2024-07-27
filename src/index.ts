18894c8f9571a9be72287aac0e81a31078bcf089
Nanndo9 <100873856+Nanndo9@users.noreply.github.com>
2024-07-27 03:34:50 -0300

    feat: add UpdateUserController with validations

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import { MongoGetUsersRepository } from './repositories/get-users/mongo-get-user
 import { MongoClient } from './database/mongo';
 import { MongoCreateUserRepository } from './repositories/create-user/mongo-create-user';
 import { CreateUserController } from './controllers/create-user/create-user';
-
+import { MongoUpdateUserRepository } from './repositories/update-user/mongo-update-user';
+import { UpdateUserController } from './controllers/update-user/update-user';
 const main = async () => {
     const app = express();
     app.use(express.json());
@@ -34,8 +35,24 @@ const main = async () => {
 
         res.status(statusCode).send(body);
     });
+    app.patch('/users/:id', async (req: Request, res: Response) => {
+        const mongoUpdateUserRepository = new MongoUpdateUserRepository();
+
+        const updateUserController = new UpdateUserController(
+            mongoUpdateUserRepository
+        );
+        const { body, statusCode } = await updateUserController.handle({
+            body: req.body,
+            params: req.params,
+        });
+        res.status(statusCode).send(body);
+    });
 
     const port = process.env.PORT;
     app.listen(port, () => console.log(`listening on port ${port}`));
 };
 main();
+/* function async(req: any, res: any): any {
+    throw new Error('Function not implemented.');
+}
+ */
